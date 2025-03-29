1d18f3dd103d1b4e5e24dd1450bd017a445c14b3
mv735065 <245119735065@mvsrec.edu.in>
2025-03-29 11:46:55 +0530

    add set to volatility filter , made some ui changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
+// e9f0fc bg-light-blue
 
-const Card = () => {
+function Card({ ele }) {
+  let subscriptionTypeIsPrivate = ele.flags.private;
+  let heading = ele.info.name.substring(0, 20) + "...";
+  let imageLink =
+    "https://assets.smallcase.com/images/smallcases/160/" + ele.scid + ".png";
+
+    let label=ele.stats.ratios.riskLabel;
+     
+
   return (
-   <>
-   </>
-  )
+    <div className="flex  border-b border-b-gray-300    gap-4 p-4 py-6">
+      <img src={imageLink} alt="" className="h-[70px]  w-[70px] ml-2" />
+      <div className="info flex-4/10 flex flex-col gap-2">
+        <header className="text-xl font-bold">
+          {heading}
+          {!subscriptionTypeIsPrivate && (
+            <span className="ml-2 px-2 rounded text-[10px]  text-blue-400 border border-gray-300">
+              Free Access{" "}
+            </span>
+          )}
+        </header>
+        <p className="text-sm">{ele.info.shortDescription}</p>
+        <p className="text-[#8f9399] text-sm">{ele.info.publisherName} </p>
+      </div>
+      <div className="amount flex flex-col justify-center items-center flex-1/10">
+        <span className="text-[#8f9399] ">Min.Amount</span>
+        <span className='font-medium'>₹{ele.stats.minInvestAmount}</span>
+      </div>
+      <div className="catagire flex flex-col justify-center items-center flex-1/10 ">
+        <span className="text-[#8f9399]  text-sm font-medium">
+          {ele.stats.ratios.cagrDuration} CAGR
+        </span>
+        <span className="text-green-500 font-medium">
+          {(ele.stats.ratios.cagr * 100).toFixed(2)}%
+        </span>
+      </div>
+
+      <div
+        className={` text-sm font-medium  flex justify-center items-center flex-2/10 `}
+      >
+        <span className=" border border-gray-300 px-4 py-1 rounded-md">
+          {ele.stats.ratios.riskLabel}
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Card
--- src/Components/MainContainter.jsx
+++ src/Components/MainContainter.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react";
-// import Card from './Card';
+import Card from "./Card";
+import NavBar from "./NavBar";
+import SideBar from "./SideBar";
 
 const MainContainter = () => {
   const [smallCasesData, setSmallCasesData] = useState([]);
   const [status, setStatus] = useState({
     subscriptionType: "Show All",
     minInvestAmount: "All",
-    voltalityType: null,
+    voltalityType: new Set(),
     investmentStrategySet: new Set(),
     sortByType: "Popularity",
-    orderBy:"High-Low"
+    orderBy: "High-Low",
   });
 
+  let subscriptionTypeOptions=["Show All", "Free Access", "Fee Based"];
+  let investmentAmountOptions=["All", "Under ₹ 5000", "Under ₹ 25000", "Under ₹ 50000"];
+  let volatilityOptions=["Low", "Medium", "High"];
   let investmentStrategyListAsMap = getListOfInvestmentStrategy(smallCasesData);
 
   useEffect(() => {
@@ -42,10 +47,15 @@ const MainContainter = () => {
   }
 
   function handleVoltality(voltalityType) {
+    let set = new Set(status.voltalityType);
+    if (set.has(voltalityType)) {
+      set.delete(voltalityType);
+    } else {
+      set.add(voltalityType);
+    }
     setStatus({
       ...status,
-      voltalityType:
-        status.voltalityType == voltalityType ? null : voltalityType,
+      voltalityType: set,
     });
   }
 
@@ -67,17 +77,13 @@ const MainContainter = () => {
       ...status,
       investmentStrategySet: set,
     });
-    // console.log(set);
   }
 
-  function handleOrderBy(orderBy){
+  function handleOrderBy(orderBy) {
     setStatus({
       ...status,
-      orderBy:orderBy,
-    })
-    console.log(status.orderBy);
-    
-
+      orderBy: orderBy,
+    });
   }
 
   let filteredSmallCasesData = getFilteredSmallCasesData(
@@ -89,7 +95,11 @@ const MainContainter = () => {
     <>
       <div>{filteredSmallCasesData.length}</div>
 
-      <NavBar handleSortByFilter={handleSortByFilter} status={status}  handleOrderBy={handleOrderBy}/>
+      <NavBar
+        handleSortByFilter={handleSortByFilter}
+        status={status}
+        handleOrderBy={handleOrderBy}
+      />
 
       <div className="flex justify-center gap-4">
         <aside className=" flex flex-col gap-4 min-w-[300px]  p-4">
@@ -114,217 +124,9 @@ const MainContainter = () => {
   );
 };
 
-function Card({ ele }) {
-  let subscriptionTypeIsPrivate = ele.flags.private;
-  let heading = ele.info.name.substring(0, 20) + "...";
-  let imageLink =
-    "https://assets.smallcase.com/images/smallcases/160/" + ele.scid + ".png";
-
-  return (
-    <div className="flex  border-b border-b-gray-300    gap-4 p-4 py-6">
-      <img src={imageLink} alt="" className="h-[70px]  w-[70px] ml-2" />
-      <div className="info flex-4/10 flex flex-col gap-2">
-        <header className="text-xl font-bold">
-          {heading}
-          {!subscriptionTypeIsPrivate && (
-            <span className="ml-2 px-2 rounded text-[10px]  text-blue-400 border border-gray-300">
-              Free Access{" "}
-            </span>
-          )}
-        </header>
-        <p className="text-sm">{ele.info.shortDescription}</p>
-        <p className="text-[#8f9399] text-sm">{ele.info.publisherName} </p>
-      </div>
-      <div className="amount flex flex-col justify-center items-center flex-1/10">
-        <span className="text-[#8f9399] ">Min.Amount</span>
-        <span>₹{ele.stats.minInvestAmount}</span>
-      </div>
-      <div className="catagire flex flex-col justify-center items-center flex-1/10 ">
-        <span className="text-[#8f9399] font-light text-sm">
-          {ele.stats.ratios.cagrDuration} CAGR
-        </span>
-        <span className="text-green-500">
-          {(ele.stats.ratios.cagr * 100).toFixed(2)}%
-        </span>
-      </div>
-
-      <div
-        className={`text-red-500   flex justify-center items-center flex-2/10 `}
-      >
-        <span className=" border border-gray-300 px-4 py-0.5 rounded-md">
-          {ele.stats.ratios.riskLabel}
-        </span>
-      </div>
-    </div>
-  );
-}
-
-function SideBar({
-  handleSubscriptionType,
-  handleInvestmentAmountFilter,
-  handleVoltality,
-  investmentStrategyListAsMap,
-  handleInvestmentStrategy,
-  status,
-}) {
-  return (
-    <>
-      {/* Subscription Type */}
-      <p className="text-xl font-bold  ">Subscription Type</p>
-      <ul
-        className={`menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box`}
-      >
-        {["Show All", "Free Access", "Fee Based"].map((ele) => {
-          return (
-            <li
-              key={ele}
-              onClick={() => handleSubscriptionType(ele)}
-              className={`p-2 ${
-                status.subscriptionType == ele ? "border border-blue-500" : null
-              }`}
-            >
-              {ele}
-            </li>
-          );
-        })}
-      </ul>
-      {/* investmentAmount */}
-      <div className="investmentAmount">
-        <p className=" text-xl font-bold "> Investment Amount</p>
-        <ul className="flex flex-col gap-4">
-          {["All", "Under ₹ 5000", "Under ₹ 25000", "Under ₹ 50000"].map(
-            (ele, index) => {
-              return (
-                <li key={ele}>
-                  <input
-                    type="radio"
-                    name="radio-1"
-                    className={`radio `}
-                    onChange={() => handleInvestmentAmountFilter(ele)}
-                    checked={status.minInvestAmount == ele}
-                  />{" "}
-                  <span className="pl-2">{ele}</span>
-                </li>
-              );
-            }
-          )}
-        </ul>
-      </div>
-      {/* Voltality */}
-      <div className="Voltality ">
-        <p className=" text-xl font-bold ">Voltality</p>
-        <ul className="flex mt-4 gap-0.5">
-          {["Low", "Medium", "High"].map((ele) => {
-            return (
-              <li key={ele}>
-                <button
-                  className={`py-1 px-4 border  ${
-                    status.voltalityType == ele
-                      ? "border-2 border-blue-500"
-                      : "border-gray-300"
-                  }`}
-                  onClick={() => handleVoltality(ele)}
-                >
-                  {ele}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-
-      <div className="Investment Strategy">
-        <p className=" text-xl font-bold ">Investment Strategy</p>
-        <ul className="flex mt-4 flex-col gap-2">
-          {...investmentStrategyListAsMap.keys().map((ele) => {
-            return (
-              <li key={ele}>
-                <input
-                  type="checkbox"
-                  checked={status.investmentStrategySet.has(ele)}
-                  onChange={() => handleInvestmentStrategy(ele)}
-                />{" "}
-                {investmentStrategyListAsMap.get(ele)}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </>
-  );
-}
-
-function NavBar({ handleSortByFilter, status ,handleOrderBy}) {
-  return (
-    <div className="dropdown dropdown-start relative w-full flex flex-col justify-center items-center">
-      <div tabIndex={0} role="button" className="btn m-1 bg-white">
-        <span className="text-gray-400 font-light">Sort By </span>{" "}
-        <span>{status.sortByType} </span>
-      </div>
-      <ul
-        tabIndex={0}
-        className="dropdown-content bg-white rounded-box w-fill p-2 shadow-sm absolute top-full mt-1 z-50"
-      >
-        {["Popularity", "Minimum Amount", "Recently Rebalanced"].map(
-          (ele, index) => (
-            <li className="flex flex-row items-center mt-2" key={ele}>
-              <input
-                type="radio"
-                className="radio radio-neutral "
-                name="radio"
-                id={`radio1${index}`}
-                onChange={() => handleSortByFilter(ele)}
-                checked={status.sortByType == ele}
-              />
-              <label htmlFor={`radio1${index}`} className="ml-2">
-                {ele}
-              </label>
-            </li>
-          )
-        )}
-        <li className="mx-2 mr-2 flex flex-col gap-2 mt-2">
-          <span>Returns</span>
-          <span>Time Period</span>
-          <ul className="flex flex-row   rounded border border-gray-300">
-            {["1M", "6M", "1Y","2Y", "3Y", "5M"].map((ele) => {
-              return (
-                <li
-                  key={ele}
-                  className={`px-4 py-1 font-bold text-sm  ${status.sortByType==ele ? 'border text-blue-400 rounded ':'text-gray-400'} `}
-                  onClick={() => handleSortByFilter(ele)}
-                >
-                  {ele}
-                </li>
-              );
-            })}
-           
-          </ul>
-        </li>
-        <li className={`my-4 mx-2`} hidden={isNaN(parseInt(status.sortByType.substring(0, 1)))}>
-  <span>Order By</span>
-  <div className="flex justify-between">
-    {["High-Low", "Low-High"].map((order) => (
-      <button
-        key={order}
-        className={`px-4 py-1 border text-sm flex-1 font-bold rounded ${
-          status.orderBy === order ? 'border text-blue-400' : 'text-gray-400'
-        }`}
-        onClick={() => handleOrderBy(order)}
-      >
-        {order}
-      </button>
-    ))}
-  </div>
-</li>
-
-      </ul>
-    </div>
-  );
-}
-
 function getFilteredSmallCasesData(status, data) {
   let filterdData = [];
-  let voltalityType = status.voltalityType;
+  let voltalityTypeSet = status.voltalityType;
   let subscriptionType = status.subscriptionType;
   let minInvestAmount = status.minInvestAmount;
   let investmentStrategy = status.investmentStrategySet;
@@ -357,10 +159,11 @@ function getFilteredSmallCasesData(status, data) {
   }
 
   // voltalityType
-  if (voltalityType) {
+  if (voltalityTypeSet && voltalityTypeSet.size!=0) {
     filterdData = filterdData.filter((ele) => {
       let volatility = ele.stats.ratios.riskLabel;
-      return volatility == voltalityType + " Volatility";
+      return voltalityTypeSet.has(volatility.split(" ")[0].trim());
+       
     });
   }
 
@@ -383,26 +186,24 @@ function getFilteredSmallCasesData(status, data) {
     filterdData = filterdData.sort((e1, e2) => {
       return e1.stats.minInvestAmount - e2.stats.minInvestAmount;
     });
-  } else if (sortByType =="Recently Rebalanced") {
+  } else if (sortByType == "Recently Rebalanced") {
     filterdData = filterdData.sort((e1, e2) => {
       const date1 = new Date(e1.info.lastRebalanced);
       const date2 = new Date(e2.info.lastRebalanced);
       return date2 - date1;
     });
   } else {
-  
     filterdData = filterdData.filter((ele) => {
-      let duration = ele.stats.ratios.cagrDuration.split('MY')[0];
+      let duration = ele.stats.ratios.cagrDuration.split("MY")[0];
       return duration == sortByType;
     });
-     
-      filterdData = filterdData.sort((e1, e2) => {
-        let value1=parseFloat((e1.stats.ratios.cagr * 100))
-        let value2=parseFloat((e2.stats.ratios.cagr * 100))
-        if(status.orderBy=='High-Low') return value2-value1;
-       else return value1-value2;
-      });
 
+    filterdData = filterdData.sort((e1, e2) => {
+      let value1 = parseFloat(e1.stats.ratios.cagr * 100);
+      let value2 = parseFloat(e2.stats.ratios.cagr * 100);
+      if (status.orderBy == "High-Low") return value2 - value1;
+      else return value1 - value2;
+    });
   }
 
   return filterdData;
